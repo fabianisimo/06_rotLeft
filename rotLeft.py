0358341d3c4c62af51d2471f1d20e234e2abd49b
Fabianisimo <fabianbravotoro@gmail.com>
2020-05-14 22:06:54 -0400

    prueba en atom

--- rotLeft.py
+++ rotLeft.py
@@ -1,13 +1,13 @@
 import random
 
 def entrada():
-    n = random.randint(1,10)
+    n = random.randint(1,9)
     a =[]
     i = 0
     while i < n:
         a.append(random.randint(1,9))
         i = i+1
-    d = random.randint(1,n)
+    d = random.randint(1,2)
     return [a,d]
 
 
@@ -17,6 +17,21 @@ print ("largo a: ", len(datos[0]))
 print ("d: ",datos[1])
 
 def rotLeft(a,d):
+    pib = []
+    quitador = 0
+    for largo in range(len(a)):
+        if largo < d:
+            pib.append(a[largo])
+        if largo < len(a)-d:
+            a[largo] = a[largo+d]
+        else:
+            a[largo] = pib[quitador]
+            quitador = quitador + 1
+    return a
+
+
+
+def rotLeft_mal(a,d):
     cant = 1
     while cant <= d:
         pib = a[0]
