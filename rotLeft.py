c2a59f9e28b63277abd6c9a6570361e3a9c8b6c5
Fabianisimo <fabianbravotoro@gmail.com>
2020-05-14 22:13:37 -0400

    final final ahora si funcionò

--- rotLeft.py
+++ rotLeft.py
@@ -10,7 +10,6 @@ def entrada():
     d = random.randint(1,2)
     return [a,d]
 
-
 datos = entrada()
 print ("a: ",datos[0])
 print ("largo a: ", len(datos[0]))
@@ -30,20 +29,5 @@ def rotLeft(a,d):
             quitador = quitador + 1
     return a
 
-
-
-def rotLeft_mal(a,d):
-    cant = 1
-    while cant <= d:
-        pib = a[0]
-        it = 0
-        while it <= len(a)-1:
-            if it == len(a)-1:
-                a[it] = pib
-            else:
-                a[it] = a[it+1]
-            it = it + 1
-        cant = cant + 1
-    return a
 
 print (rotLeft(datos[0],datos[1]))
